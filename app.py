b46b2b4cf82cffed8d99fc8c5089f24bc07ddb53
mj <21112002mj@gmail.com>
2022-03-09 21:21:19 +0530

    flask framework added

---
+++ app.py
@@ -0,0 +1,24 @@
+ 
+from unicodedata import name
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+details = [
+    ("Parth","R55","890013332","Free time")
+]
+
+@app.route("/form", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+
+     print(request.form)
+     details.append((
+         request.form.get("name"),
+         request.form.get("room"),
+         request.form.get("phone"),
+         request.form.get("details")))
+    return render_template("form.html")
+
+@app.route("/")
+def display_details():
+    return render_template("index.html", entries = details)
